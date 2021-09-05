c2e650bf6878ea9a82f999978622371f1a9cb27a
MarianoLazzos <lazzos97@gmail.com>
2021-09-05 17:51:00 -0300

    get de comercios

---
+++ Trabajos Practicos/Evaluables/TP6/ISW-2021-TPE-G10-06-DeliverEat/server/routes/comerceRoutes.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Comercio = mongoose.model('comercio')
+
+module.exports = app => {
+  app.get('./comercios', async (req, res) => {
+    const comercios = await Comercio.find();
+
+    res.send(comercios);
+  })
+
+}
